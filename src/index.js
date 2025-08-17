6b5d832aba738a0b477a2ada7f50172c26b6d3b4
Kristina Tymoshenko <Timoshenko250998@gmail.com>
2025-08-17 23:40:35 +0300

    Added basic form handling and added Typewriter js plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-// alert("Hello World🤓")
+
+function generatePoem(event) {
+    event.preventDefault();
+
+
+    new Typewriter('#poem', {
+        strings: "Text of generated poem",
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+      });
+    
+}
+
+
+
+
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
